f8dd35e47d7b3fd35ae03f952cbfea721616a5c6
WesleyReis13 <lopeswesley1392@gmail.com>
2025-09-09 23:41:32 -0300

    feat: form-triagem

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -6,14 +6,14 @@ const Container: React.FC = () => {
   return (
     <div className="w-auto bg-[#F3EDE2] p-6 flex flex-col gap-4">
       <h1 className="text-xl font-bold text-[#49543A] ">Descubra em minutos</h1>
-      <p className="text-[#49543A] ">Se o nosso produto é para você</p>
+      <p className="text-[#49543A] ">Se o nosso serviço é para você</p>
       <ul className="list-disc  pl-6 pr-50">
         <li className="text-[#49543A]">Teste teste testes testes testes testes </li>
         <li className="text-[#49543A]">Teste teste testes testes testes testes</li>
         <li className="text-[#49543A]">Teste teste testes testes testes testes</li>
       </ul>
       <ProfileCard
-        photo="https://via.placeholder.com/150"
+        photo="https://picsum.photos/300/200"
         name="John Doe"
         role="CEO"
         social1="https://linkedin.com"
