d400d02dcc6522e91116e3d427db07e78f6467cd
WesleyReis13 <lopeswesley1392@gmail.com>
2025-09-11 03:49:41 -0300

    feat: painel-leads

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,12 @@ import Container from '../components/Container'
 import ContainerForm from '../components/ContainerForm'
 import MultiStepForm from "../components/MultiStepForms/Index";
 import SignUp from "./login/SignUp";
+import CrudDashboard from "./dashboard/painel/CrudDashboard";
 
 
 
 export default function Home(){
     return(
-    <div className="min-h-screen flex justify-between items-center gap-6 ">
-      <Container/>
-      <MultiStepForm/>
-    </div>
+      <CrudDashboard/>
     )
 }
